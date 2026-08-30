6e99bf4b9438b46918a96fe326e9acd8d62dd65f
Ajitesh Dakua <157983370+AjiteshDakua@users.noreply.github.com>
2026-08-30 20:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (30.42%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,5 +1,10 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         n = len(nums)
 
         lb = n
@@ -16,4 +21,3 @@ class Solution:
                 low = mid + 1
 
         return lb
-
